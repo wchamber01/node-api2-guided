9a8d7465d9de431afbf62c5a4a8eef9761d689d2
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-14 12:06:41 +0100

    wip

---
+++ hubs/hubs-router.js
@@ -0,0 +1,103 @@
+// in order to create a router we need express
+const express = require('express')
+
+// we need to import any other dependencies for this module
+const Hubs = require('./hubs-model')
+
+// this is how we instantiate a router
+const router = express.Router();
+
+router.get('/api/hubs', (req, res) => {
+  Hubs.find(req.query)
+  .then(hubs => {
+    res.status(200).json(hubs);
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the hubs',
+    });
+  });
+});
+
+router.get('/api/hubs/:id', (req, res) => {
+  console.log(req.body);
+  console.log(req.params);
+  console.log(req.query);
+  // 3 main things we can fish from req:
+
+  // - url parameters req.params.id
+  // - body req.body
+  // - query string http://google.com?search=cats req.query.search
+  Hubs.findById(req.params.id)
+  .then(hub => {
+    if (hub) {
+      res.status(200).json(hub);
+    } else {
+      res.status(404).json({ message: 'Hub not found' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the hub',
+    });
+  });
+});
+
+router.post('/api/hubs', (req, res) => {
+  Hubs.add(req.body)
+  .then(hub => {
+    res.status(201).json(hub);
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the hub',
+    });
+  });
+});
+
+router.delete('/api/hubs/:id', (req, res) => {
+  Hubs.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({ message: 'The hub has been nuked' });
+    } else {
+      res.status(404).json({ message: 'The hub could not be found' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the hub',
+    });
+  });
+});
+
+router.put('/api/hubs/:id', (req, res) => {
+  const changes = req.body;
+  Hubs.update(req.params.id, changes)
+  .then(hub => {
+    if (hub) {
+      res.status(200).json(hub);
+    } else {
+      res.status(404).json({ message: 'The hub could not be found' });
+    }
+  })
+  .catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the hub',
+    });
+  });
+});
+
+// expose the router to the outside world
+module.exports = router;
+
